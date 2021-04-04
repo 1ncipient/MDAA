bec9a75a8d89bbf82aef99f4e506c5513ff1e524
Yan Qing Niu <yniu54@uwo.ca>
2021-04-04 04:11:56 -0400

    Added SelectionObject, AnalysisObject, and Calculate

--- src/main/java/analysis/Calculate.java
+++ src/main/java/analysis/Calculate.java
@@ -1,5 +1,18 @@
 package analysis;
 
+import selection.SelectionObject;
+
+/**
+ * Interface exposed by AnalysisObject. Needed for communication with it.
+ * @author Yan Qing Niu
+ *
+ */
 public interface Calculate {
-	public void calculate(SelectionObject select);
+	
+	/**
+	 * Abstract method, implemented by AnalysisObject
+	 * @param select New SelectionObject for current AnalysisObject
+	 */
+	public void calculate(selection.SelectionObject select);
+
 }
